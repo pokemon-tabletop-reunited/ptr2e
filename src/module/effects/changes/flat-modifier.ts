37c00a0e0a335a6660a2cae650abcc03e0dcdaa1
Ashe <ashe@soaringnetwork.com>
2024-07-23 17:37:20 +0200

    Fix flat modifier not manually taking method suffixes into account

--- src/module/effects/changes/flat-modifier.ts
+++ src/module/effects/changes/flat-modifier.ts
@@ -3,79 +3,94 @@ import { ChangeModel } from "@data";
 import { ModifierPTR2e } from "../modifiers.ts";
 
 export default class FlatModifierChangeSystem extends ChangeModel {
-    static override TYPE = "flat-modifier";
+  static override TYPE = "flat-modifier";
 
-    static override defineSchema() {
-        const fields = foundry.data.fields;
-        return {
-            ...super.defineSchema(),
-            hideIfDisabled: new fields.BooleanField(),
-        };
-    }
+  static override defineSchema() {
+    const fields = foundry.data.fields;
+    return {
+      ...super.defineSchema(),
+      hideIfDisabled: new fields.BooleanField(),
+    };
+  }
 
-    get selector() {
-        return this.key;
-    }
+  get selector() {
+    return this.key;
+  }
 
-    override apply(actor: ActorPTR2e): void {
-        this.beforePrepareData(actor);
-    }
+  override apply(actor: ActorPTR2e): void {
+    this.beforePrepareData(actor);
+  }
 
-    override beforePrepareData(
-        actor: ActorPTR2e | null = this.actor,
-        method: ModifierPTR2e["method"] = "flat"
-    ): void {
-        if (this.ignored) return;
-        if (!actor) return;
+  override beforePrepareData(
+    actor: ActorPTR2e | null = this.actor,
+    providedMethod: ModifierPTR2e["method"] = "flat"
+  ): void {
+    if (this.ignored) return;
+    if (!actor) return;
 
-        const label = this.getReducedLabel();
+    const label = this.getReducedLabel();
 
-        const resolvedSelector = this.resolveInjectedProperties(this.key);
-        if (!resolvedSelector) return;
+    const resolvedSelector = this.resolveInjectedProperties(this.key);
+    if (!resolvedSelector) return;
 
-        const { selector, type } = ((): { selector: string; type: ModifierPTR2e["type"] } => {
-            const suffixes = ["accuracy", "evasion", "damage", "crit", "power"] as const;
+    const { selector, type, method } = ((): { selector: string; type: ModifierPTR2e["type"], method: ModifierPTR2e["method"] } => {
+      const { selector, method } = (() => {
+        const methodSuffixes = ["percentile", "stage", "base", "flat"] as const;
 
-            for (const suffix of suffixes) {
-                if (resolvedSelector.endsWith("-" + suffix)) {
-                    return {
-                        selector: resolvedSelector.replace("-" + suffix, ""),
-                        type: suffix,
-                    };
-                }
-            }
-            return { selector: resolvedSelector, type: "any" };
-        })();
+        for (const suffix of methodSuffixes) {
+          if (resolvedSelector.endsWith("-" + suffix)) {
+            return {
+              selector: resolvedSelector.replace("-" + suffix, ""),
+              method: suffix,
+            };
+          }
+        }
+        return { selector: resolvedSelector, method: providedMethod };
+      })();
 
-        const slug = `${this.slug}-${type}-${method}`;
+      const suffixes = ["accuracy", "evasion", "damage", "crit", "power"] as const;
 
-        const construct = (options: DeferredValueParams = {}): ModifierPTR2e | null => {
-            const resolvedValue = Number(this.resolveValue(this.value, 0, options));
-            if (this.ignored || isNaN(resolvedValue)) return null;
+      for (const suffix of suffixes) {
+        if (selector.endsWith("-" + suffix)) {
+          return {
+            selector: selector.replace("-" + suffix, ""),
+            type: suffix,
+            method
+          };
+        }
+      }
+      return { selector: selector, type: "any", method };
+    })();
 
-            const predicate = this.predicate.clone();
+    const slug = `${this.slug}-${type}-${method}`;
 
-            const modifier = new ModifierPTR2e({
-                slug,
-                label,
-                modifier: resolvedValue,
-                predicate: this.resolveInjectedProperties(predicate),
-                change: this,
-                hideIfDisabled: this.hideIfDisabled,
-                method,
-                type,
-                //TODO: Add method of application
-            });
-            if (options.test) modifier.test(options.test);
+    const construct = (options: DeferredValueParams = {}): ModifierPTR2e | null => {
+      const resolvedValue = Number(this.resolveValue(this.value, 0, options));
+      if (this.ignored || isNaN(resolvedValue)) return null;
 
-            return modifier;
-        };
+      const predicate = this.predicate.clone();
 
-        const modifiers = (actor.synthetics.modifiers[selector] ??= []);
-        modifiers.push(construct);
-    }
+      const modifier = new ModifierPTR2e({
+        slug,
+        label,
+        modifier: resolvedValue,
+        predicate: this.resolveInjectedProperties(predicate),
+        change: this,
+        hideIfDisabled: this.hideIfDisabled,
+        method,
+        type,
+        //TODO: Add method of application
+      });
+      if (options.test) modifier.test(options.test);
+
+      return modifier;
+    };
+
+    const modifiers = (actor.synthetics.modifiers[selector] ??= []);
+    modifiers.push(construct);
+  }
 }
 
 export default interface FlatModifierChangeSystem {
-    hideIfDisabled: boolean;
+  hideIfDisabled: boolean;
 }
