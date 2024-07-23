7ac6da4ec15535bb1f9755e7ec1e14f3bc9ebb16
Ashe <ashe@soaringnetwork.com>
2024-07-23 16:50:31 +0200

    Disable effect automation from unslotted non-free abilities
    Resolves #473

--- src/module/item/data/ability.ts
+++ src/module/item/data/ability.ts
@@ -8,54 +8,62 @@ import { BaseItemSourcePTR2e, ItemSystemSource } from "./system.ts";
  * @extends {foundry.abstract.TypeDataModel}
  */
 export default abstract class AbilitySystem extends HasEmbed(HasBase(foundry.abstract.TypeDataModel), "ability") {
-    /**
-     * @internal
-     */
-    declare parent: AbilityPTR2e;
+  /**
+   * @internal
+   */
+  declare parent: AbilityPTR2e;
 
-    static override defineSchema(): AbilitySchema {
-        const fields = foundry.data.fields;
-        return {
-            ...super.defineSchema(),
+  static override defineSchema(): AbilitySchema {
+    const fields = foundry.data.fields;
+    return {
+      ...super.defineSchema(),
 
-            free: new fields.BooleanField({
-                required: true,
-                initial: false,
-                label: "PTR2E.FIELDS.free.label",
-                hint: "PTR2E.FIELDS.free.hint",
-            }),
-            slot: new fields.NumberField({
-                required: true,
-                nullable: true,
-                initial: null,
-                label: "PTR2E.FIELDS.slot.label",
-                hint: "PTR2E.FIELDS.slot.hint",
-            }),
-        };
+      free: new fields.BooleanField({
+        required: true,
+        initial: false,
+        label: "PTR2E.FIELDS.free.label",
+        hint: "PTR2E.FIELDS.free.hint",
+      }),
+      slot: new fields.NumberField({
+        required: true,
+        nullable: true,
+        initial: null,
+        label: "PTR2E.FIELDS.slot.label",
+        hint: "PTR2E.FIELDS.slot.hint",
+      }),
+    };
+  }
+
+  override prepareBaseData(): void {
+    if (!this.free && this.slot === null) {
+      this.parent.effects.map(e => e.disabled = true);
     }
 
-    override async _preCreate(data: this["parent"]["_source"], options: DocumentModificationContext<this["parent"]["parent"]>, user: User): Promise<boolean | void> {
-        const result = await super._preCreate(data, options, user);
-        if (result === false) return false;
+    super.prepareBaseData();
+  }
 
-        if(!data.img || data.img === "icons/svg/item-bag.svg") {
-            this.parent.updateSource({
-                img: "/systems/ptr2e/img/icons/ability_icon.webp"
-            })
-        }
+  override async _preCreate(data: this["parent"]["_source"], options: DocumentModificationContext<this["parent"]["parent"]>, user: User): Promise<boolean | void> {
+    const result = await super._preCreate(data, options, user);
+    if (result === false) return false;
+
+    if (!data.img || data.img === "icons/svg/item-bag.svg") {
+      this.parent.updateSource({
+        img: "/systems/ptr2e/img/icons/ability_icon.webp"
+      })
     }
+  }
 }
 
-export default interface AbilitySystem extends ModelPropsFromSchema<AbilitySchema> {}
+export default interface AbilitySystem extends ModelPropsFromSchema<AbilitySchema> { }
 
 type AbilitySchema = {
-    slot: foundry.data.fields.NumberField<number, number, true, true, true>;
-    free: foundry.data.fields.BooleanField<boolean, boolean, true, false, true>;
+  slot: foundry.data.fields.NumberField<number, number, true, true, true>;
+  free: foundry.data.fields.BooleanField<boolean, boolean, true, false, true>;
 }
 
 export type AbilitySource = BaseItemSourcePTR2e<"ability", AbilitySystemSource>;
 
 interface AbilitySystemSource extends ItemSystemSource {
-    slot: number;
-    free: boolean;
+  slot: number;
+  free: boolean;
 }
