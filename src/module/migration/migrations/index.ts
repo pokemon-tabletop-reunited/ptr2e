c2bd29ba4fc4a7e353b73f16f8d73b72b69db41e
Dylan McInnes <dylan.c.mcinnes@gmail.com>
2024-10-24 12:56:26 -0600

    Migration script

--- src/module/migration/migrations/index.ts
+++ src/module/migration/migrations/index.ts
@@ -5,3 +5,4 @@ export { Migration104FolderUpdate } from "./104-folder-update.ts";
 export { Migration105GearIcons } from "./105-gear-icons.ts";
 export { Migration106AbilityUUIDs } from "./106-ability-uuids.ts";
 export { Migration107AbsolutePaths } from "./107-absolute-paths.ts";
+export { Migration108ConsumableStack } from "./108-consumable-stack.ts";
