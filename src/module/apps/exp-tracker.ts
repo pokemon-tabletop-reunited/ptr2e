33cf252bbeece8ccdaf1636946a096170dc4e87d
Ashe <ashe@soaringnetwork.com>
2024-12-19 16:41:06 +0100

    Add Basic Training EXP Tab
    Resolves #853

--- src/module/apps/exp-tracker.ts
+++ src/module/apps/exp-tracker.ts
@@ -21,7 +21,7 @@ export class EXPTracker extends foundry.applications.api.HandlebarsApplicationMi
         minimizable: true,
         resizable: true,
       },
-      // dragDrop: [{ dragSelector: null, dropSelector: '.window-content' }],
+      dragDrop: [{ dragSelector: ".box .actor:not(.loafing)", dropSelector: ".training-targets .actor" }],
       actions: {
         "add-modifier": async function (this: EXPTracker) {
           const labelInput = this.element.querySelector<HTMLInputElement>("input[name='customName']");
@@ -39,7 +39,7 @@ export class EXPTracker extends foundry.applications.api.HandlebarsApplicationMi
           if (!id) return;
           return game.settings.get("ptr2e", "expTrackerData").remove(id).then(() => this.render({ parts: ["venture"] }));
         },
-        "apply-exp": async function (this: EXPTracker, event: MouseEvent) {
+        "apply-venture-exp": async function (this: EXPTracker, event: MouseEvent) {
           if (!game.user?.isGM) return;
 
           const button = (event.target as HTMLButtonElement);
@@ -48,7 +48,7 @@ export class EXPTracker extends foundry.applications.api.HandlebarsApplicationMi
           button.classList.add("disabled");
           ui.notifications.info("Applying exp...");
 
-          const { total, cmsPercent } = this.prepareModifiers();
+          const { total, cmsPercent } = this._prepareModifiers();
           const characters = this.characters;
 
           const { updates, messages, undoData } = characters.reduce((acc, character) => {
@@ -80,6 +80,57 @@ export class EXPTracker extends foundry.applications.api.HandlebarsApplicationMi
           })
 
           await this.render({ parts: ["venture", "footer"] });
+        },
+        "apply-training-exp": async function (this: EXPTracker, event: MouseEvent) {
+          if (!game.user?.isGM) return;
+
+          const button = (event.target as HTMLButtonElement);
+          if (!button) return;
+          button.disabled = true;
+          button.classList.add("disabled");
+          ui.notifications.info("Applying exp...");
+
+          const total = this.ber;
+          if(!total) return;
+          const characters = Object.values(this.slots).flatMap(slot => slot.flatMap(s => s.actor ?? []));
+          if(!characters.length) {
+            ui.notifications.error("No characters selected");
+            return void this.render({ parts: ["footer"] });
+          }
+
+          const { updates, messages, undoData } = characters.reduce((acc, character) => {
+            const newTotal = character.system.advancement.experience.current + total;
+            const levelUp = character.system.advancement.experience.next <= newTotal;
+            acc.undoData.push({ _id: character.id, "system.advancement.experience.current": character.system.advancement.experience.current });
+            acc.updates.push({
+              _id: character.id,
+              "system.advancement.experience.current": newTotal
+            });
+            acc.messages.push({ link: character.link, new: newTotal, old: character.system.advancement.experience.current, levelUp });
+            return acc;
+          }, {
+            undoData: [],
+            updates: [],
+            messages: []
+          } as Record<string, Record<string, unknown>[]>);
+
+          await ActorPTR2e.updateDocuments(updates);
+          
+          this.slots = {};
+          this.openDetails = null;
+          game.user.unsetFlag("ptr2e", "exp-training-slots");
+
+          await ChatMessage.create({
+            content: await renderTemplate("systems/ptr2e/templates/chat/exp-tracker.hbs", { messages, total }),
+            speaker: ChatMessage.getSpeaker({ alias: game.i18n.localize("PTR2E.ExpTracker.title") }),
+            flags: {
+              ptr2e: {
+                undoData
+              }
+            },
+          })
+
+          await this.render({ parts: ["training", "footer"] });
         }
       }
     },
@@ -145,6 +196,12 @@ export class EXPTracker extends foundry.applications.api.HandlebarsApplicationMi
     }
     return this.tabs;
   }
+  
+  override changeTab(tab: string, group: string, options?: { event?: Event; navElement?: HTMLElement; force?: boolean; updatePosition?: boolean; }): void {
+    super.changeTab(tab, group, options);
+
+    this.render({parts: ["footer"]});
+  }
 
   private ber: number;
   private characters: ActorPTR2e[] = [];
@@ -172,7 +229,7 @@ export class EXPTracker extends foundry.applications.api.HandlebarsApplicationMi
         folders,
         entries: entries.map(entry => ({
           actor: entry,
-          loafing: (entry).effects.get("loafingafflictio") as ActiveEffectPTR2e
+          loafing: entry.effects.get("loafingafflictio") as ActiveEffectPTR2e
         }))
       }
     });
@@ -183,17 +240,20 @@ export class EXPTracker extends foundry.applications.api.HandlebarsApplicationMi
       const existing = acc.find(c => c.pc.folder?.id && c.pc.folder.id === pc.folder?.id);
       if (existing) {
         if (pc.system.party.ownerOf === existing.pc.folder!.id) {
+          delete this.slots[existing.pc.uuid];
           existing.pc = pc;
+          existing.slots = this._getSlots(pc);
         }
         return acc;
       }
 
       acc.push({
         pc,
-        data: pc.folder ? getBoxData(pc.folder) : { folders: [], entries: [] }
+        data: pc.folder ? getBoxData(pc.folder) : { folders: [], entries: [] },
+        slots: this._getSlots(pc)
       });
       return acc;
-    }, [] as { pc: ActorPTR2e, data: { folders: Folder[], entries: { actor: ActorPTR2e, loafing?: ActiveEffectPTR2e }[] } }[]);
+    }, [] as { pc: ActorPTR2e, data: { folders: Folder[], entries: { actor: ActorPTR2e, loafing?: ActiveEffectPTR2e }[] }, slots: { actor?: ActorPTR2e }[] }[]);
 
     const apl = pcs.reduce((acc, pc, index, arr) => {
       acc += pc.level
@@ -208,12 +268,15 @@ export class EXPTracker extends foundry.applications.api.HandlebarsApplicationMi
     // Base Experience Reward == 25% of the experience required to reach the next level for the average party level
     const ber = this.ber = Math.ceil((expNextLevel - expMinimumApl) * 0.25);
 
-    const { circumstanceGroups, cmsPercent, total } = this.prepareModifiers();
+    const { circumstanceGroups, cmsPercent, total } = this._prepareModifiers();
 
     this.characters = characters.reduce((acc, { pc, data }) => {
       acc.push(pc, ...data.entries.flatMap(entry => entry.loafing ? [] : entry.actor));
       return acc;
     }, [] as ActorPTR2e[]);
+
+    const open = this.openDetails;
+    this.openDetails = null;
 
     return {
       ...super._prepareContext(options),
@@ -222,11 +285,12 @@ export class EXPTracker extends foundry.applications.api.HandlebarsApplicationMi
       tabs: this._getTabs(),
       circumstanceGroups,
       cmsPercent,
-      total
+      total,
+      open
     }
   }
 
-  prepareModifiers() {
+  _prepareModifiers() {
     const circumstanceGroups = fu.duplicate(CONFIG.PTR.data.circumstanceModifierGroups);
     const setting = game.settings.get("ptr2e", "expTrackerData");
     for (const group in circumstanceGroups) {
@@ -250,6 +314,15 @@ export class EXPTracker extends foundry.applications.api.HandlebarsApplicationMi
     }
   }
 
+  private slots: Record<string, { actor?: ActorPTR2e }[]>;
+  private openDetails: string | null = null;
+
+  _getSlots(pc: ActorPTR2e) {
+    if(!this.slots) this.slots = game.user.getFlag("ptr2e", "exp-training-slots") as Record<string, {actor?: ActorPTR2e}[]> ?? {};
+
+    return this.slots[pc.uuid.replaceAll(".","-")] ?? (this.slots[pc.uuid.replaceAll(".","-")] = [{}, {}, {}, {}, {}, {}]);
+  }
+
   override _attachPartListeners(partId: string, htmlElement: HTMLElement, options: HandlebarsRenderOptions): void {
     super._attachPartListeners(partId, htmlElement, options);
 
@@ -265,11 +338,22 @@ export class EXPTracker extends foundry.applications.api.HandlebarsApplicationMi
 
           await setting.update(id, checked);
 
-          const { cmsPercent, total } = this.prepareModifiers();
+          const { cmsPercent, total } = this._prepareModifiers();
           const percentElement = htmlElement.querySelector<HTMLElement>("[data-name='cms-percent']");
           const expTotalElement = htmlElement.querySelector<HTMLElement>("[data-name='exp-total']");
           if (percentElement) percentElement.textContent = `${(cmsPercent * 100).toFixed(0)}%`;
           if (expTotalElement) expTotalElement.textContent = total.toString();
+        });
+      }
+    }
+    if (partId === "training") {
+      for (const element of htmlElement.querySelectorAll<HTMLDivElement>(".slot .actor:not(.empty)")) {
+        element.addEventListener("contextmenu", () => {
+          const slot = parseInt(element.parentElement?.dataset.slot ?? "");
+          if (isNaN(slot)) return;
+
+          this.slots[element.dataset.uuid!][slot].actor = undefined;
+          return this.render({ parts: ["training"] })
         });
       }
     }
@@ -296,14 +380,46 @@ export class EXPTracker extends foundry.applications.api.HandlebarsApplicationMi
     }
   }
 
+  override _onDragStart(event: DragEvent): void {
+    const target = event.currentTarget as HTMLElement;
+    if (!target.classList.contains("actor")) return;
+
+    const uuid = target.dataset.uuid;
+    if (!uuid) return;
+
+    event.dataTransfer?.setData("text/plain", JSON.stringify({ type: 'Actor', uuid }));
+  }
+
   override async _onDrop(event: DragEvent) {
     event.preventDefault();
-    // const data: { uuid: string, type: string } = TextEditor.getDragEventData(event);
-    // if (data.type !== "Actor" || !data.uuid) return;
 
-    // const actor = await fromUuid<ActorPTR2e>(data.uuid);
-    // if (!actor) return;
-    // this.render({ actor, parts: ["aside", "list"] });
+    const target = event.currentTarget as HTMLElement;
+    if (!target.classList.contains("actor")) return;
+
+    const targetUuid = target.parentElement?.dataset.parent?.replaceAll(".","-");
+    if (!targetUuid || !this.slots[targetUuid]) return;
+
+    const slot = parseInt(target.parentElement?.dataset.slot ?? "");
+    if (isNaN(slot)) return;
+
+    const data = TextEditor.getDragEventData(event) as { type: string, uuid: string };
+    if (!(data?.type === "Actor" && data.uuid)) return;
+
+    const actor = await fromUuid<ActorPTR2e>(data.uuid);
+    if (!actor) return;
+
+    const slotElement = this.element.querySelector<HTMLDivElement>(`.slot[data-slot="${slot}"]`);
+    if (!slotElement) return;
+
+    for(const slot of this.slots[targetUuid]) {
+      if(slot.actor?.id === actor.id) slot.actor = undefined;
+    }
+    this.slots[targetUuid][slot].actor = actor;
+    game.user.setFlag("ptr2e", "exp-training-slots", this.slots);
+
+    this.openDetails = target.parentElement!.dataset.parent!;
+
+    return this.render({ parts: ["training"] })
   }
 }
 
