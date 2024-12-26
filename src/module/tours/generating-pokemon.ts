10b9e8f893648c64319abb0c1dbd25ddd08de6ac
Ashe <ashe@soaringnetwork.com>
2024-12-26 14:16:57 +0100

    Add (basic) Pokemon Generation Tour
    Resolves #354

--- src/module/tours/generating-pokemon.ts
+++ src/module/tours/generating-pokemon.ts
@@ -1,7 +1,36 @@
+import { BlueprintSheetPTR2e } from "@item/sheets/index.ts";
 import { PTRTour } from "./base.ts";
+import { BlueprintPTR2e, ItemPTR2e } from "@item";
 
 export class GeneratingPokemonTour extends PTRTour {
+  private blueprintApp: foundry.applications.api.ApplicationV2 | undefined;
+  
+  override get app() {
+    return this.blueprintApp;
+  }
+
   protected override async _preStep(): Promise<void> {
-    
+    if(this.currentStep?.id === "example" && !this.blueprintApp) {
+      //@ts-expect-error - Type check fails.
+      this.blueprintApp = await new BlueprintSheetPTR2e({
+        document: new ItemPTR2e({
+          name: "Bulbasaur",
+          type: "blueprint",
+          system: {
+            blueprints: [{
+              species: "Compendium.ptr2e.core-species.Item.pCSOFZ2UvRx8QTGs"
+            }]
+          }
+        }) as BlueprintPTR2e,
+        generation: {
+          x: 0,
+          y: 0,
+          canvas: canvas,
+          temporary: true
+        }
+      }).render(true);
+    }
+
+    await super._preStep();
   }
 }
