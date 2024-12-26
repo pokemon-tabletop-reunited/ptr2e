de5bcdaea5f179833fae4317f5e28ffcedb75cad
Ashe <ashe@soaringnetwork.com>
2024-12-26 15:08:03 +0100

    Update Actor Sheet Tour

--- src/module/tours/actor-sheet.ts
+++ src/module/tours/actor-sheet.ts
@@ -14,6 +14,7 @@ export class ActorSheetTour extends PTRTour {
 
   protected override async _preStep(): Promise<void> {
     if (!this.actor) {
+      //@ts-expect-error - Correct data is being passed
       this.actor = await ActorPTR2e.create({
         name: "Tour-san",
         type: "humanoid",
@@ -109,7 +110,53 @@ export class ActorSheetTour extends PTRTour {
             "effects": []
           }
         ],
-        effects: []
+        effects: [
+          {
+            "_id": "faintedcondition",
+            "name": "Fainted",
+            "img": "systems/ptr2e/img/conditions/fainted.svg",
+            "type": "passive",
+            "description": "<p>When a creature's HP reaches and remains at 0, it is afflicted with Fainted.</p><p>A Fainted creature cannot use Actions, does not benefit from Abilities, and is cured of all @Trait[major-affliction], @Trait[minor-affliction], @Trait[pseudo-affliction], and @Trait[stage-change] effects they possess.</p><p>Fainted is cured when the afflicted creature's HP is greater than 0.</p><p>The creature's @Affliction[weary] count increases by 1 whenever they are cured of Fainted.</p>",
+            "system": {
+              "changes": [
+                {
+                  "type": "roll-option",
+                  "domain": "effect",
+                  "value": "fainted",
+                  "key": "",
+                  "mode": 2,
+                  "priority": null,
+                  "predicate": [],
+                  "ignored": false,
+                  "suboptions": [],
+                  "state": true
+                }
+              ],
+              "removeOnRecall": false,
+              "removeAfterCombat": false,
+              "slug": null,
+              "traits": [],
+              "stacks": 0
+            },
+            "statuses": [
+              "dead"
+            ],
+            "changes": [
+              {
+                "type": "roll-option",
+                "domain": "effect",
+                "value": "fainted",
+                "key": "",
+                "mode": 2,
+                "priority": null,
+                "predicate": [],
+                "ignored": false,
+                "suboptions": [],
+                "state": true
+              }
+            ]
+          }
+        ]
       }, { temporary: true });
     }
 
@@ -117,18 +164,18 @@ export class ActorSheetTour extends PTRTour {
 
     switch (this.currentStep?.id) {
       case "stats-editor": {
-        if(!this.app?.rendered) await this.app.render(true);
+        if (!this.app?.rendered) await this.app.render(true);
         this._statsEditor = await new StatsForm({ document: this.actor }).render(true)
         break;
       }
       case "known-attacks": {
-        if(!this.app?.rendered) await this.app.render(true);
+        if (!this.app?.rendered) await this.app.render(true);
         //@ts-expect-error - Params are unnecessary
         this._actionsEditor = await this.app.options.actions["edit-movelist"].call(this.app);
         break;
       }
       case "edit-skills": {
-        if(!this.app?.rendered) await this.app.render(true);
+        if (!this.app?.rendered) await this.app.render(true);
         //@ts-expect-error - Params are unnecessary
         this._skillsEditor = await this.app.options.actions["edit-skills"].call(this.app);
         break;
@@ -137,7 +184,7 @@ export class ActorSheetTour extends PTRTour {
   }
 
   protected override async _postStep(): Promise<void> {
-    switch(this.currentStep?.id) {
+    switch (this.currentStep?.id) {
       case "stats-editor": {
         if (this._statsEditor) this._statsEditor.close();
         break;
