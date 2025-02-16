3d710af15838f52096fa4a47b722d14808af1ca4
Ashe <ashe@soaringnetwork.com>
2025-02-16 18:02:54 +0100

    Add Slider to Exp Tracker
    Resolves #1106

--- src/module/system/exp-tracker-model.ts
+++ src/module/system/exp-tracker-model.ts
@@ -1,19 +1,27 @@
 import { CollectionField } from "@module/data/fields/collection-field.ts";
 
-export const TutorListVersion = 1 as const;
+export const TutorListVersion = 2 as const;
 
 export class ExpTrackerSettings extends foundry.abstract.DataModel {
   static override defineSchema(): ExpTrackerSettingsSchema {
     return {
       data: new CollectionField(new foundry.data.fields.SchemaField({
         id: new foundry.data.fields.StringField({ required: true, nullable: false }),
+        /**
+         * @obsolete - Do not use, will be removed in a future patch.
+         */
         checked: new foundry.data.fields.BooleanField({ required: true, nullable: false, initial: false }),
+        amount: new foundry.data.fields.NumberField({ required: true, nullable: false, initial: 0, min: 0, max: 20, step: 1}),
       }), "id"),
       custom: new CollectionField(new foundry.data.fields.SchemaField({
         id: new foundry.data.fields.StringField({ required: true, nullable: false }),
+        /**
+         * @obsolete - Do not use, will be removed in a future patch.
+         */
         checked: new foundry.data.fields.BooleanField({ required: true, nullable: false, initial: false }),
         label: new foundry.data.fields.StringField({ required: true, nullable: false }),
         value: new foundry.data.fields.NumberField({ required: true, nullable: false }),
+        amount: new foundry.data.fields.NumberField({ required: true, nullable: false, initial: 0, min: 0, max: 20, step: 1}),
       }), "id"),
     }
   }
@@ -36,25 +44,25 @@ export class ExpTrackerSettings extends foundry.abstract.DataModel {
     if(this.custom.has(id)) return;
 
     const data = this.toObject();
-    data.custom.push({ id, value: value/100, label, checked: false });
+    data.custom.push({ id, value: value/100, label, amount: 0});
 
     return game.settings.set("ptr2e", "expTrackerData", data);
   }
 
-  async update(id: string, checked: boolean) {
+  async update(id: string, amount: number) {
     if(id.startsWith("custom.")) {
       const data = this.toObject();
       const custom = data.custom.find(d => d.id == id);
       if(!custom) return;
 
-      custom.checked = checked;
+      custom.amount = amount;
       return game.settings.set("ptr2e", "expTrackerData", data);
     }
     else {
       const data = this.toObject();
       const entry = data.data.find(d => d.id == id);
-      if(entry) entry.checked = checked;
-      else data.data.push({ id, checked });
+      if(entry) entry.amount = amount;
+      else data.data.push({ id, amount });
 
       return game.settings.set("ptr2e", "expTrackerData", data);
     }
@@ -69,7 +77,7 @@ interface ExpTrackerSettingsSchema extends foundry.data.fields.DataSchema {
   data: CollectionField<
     foundry.data.fields.SchemaField<ExpTrackerDataSchema>,
     SourceFromSchema<ExpTrackerDataSchema>[],
-    Collection<{ id: string, checked: boolean }>,
+    Collection<{ id: string, amount: number }>,
     true,
     false,
     true
@@ -77,7 +85,7 @@ interface ExpTrackerSettingsSchema extends foundry.data.fields.DataSchema {
   custom: CollectionField<
     foundry.data.fields.SchemaField<ExpTrackerCustomDataSchema>,
     SourceFromSchema<ExpTrackerCustomDataSchema>[],
-    Collection<{ id: string, checked: boolean, label: string, value: number }>,
+    Collection<{ id: string, amount: number, label: string, value: number }>,
     true,
     false,
     true
@@ -86,10 +94,10 @@ interface ExpTrackerSettingsSchema extends foundry.data.fields.DataSchema {
 
 interface ExpTrackerDataSchema extends foundry.data.fields.DataSchema {
   id: foundry.data.fields.StringField<string, string, true, false, false>,
-  checked: foundry.data.fields.BooleanField<boolean, boolean, true, false, true>,
 }
 
 interface ExpTrackerCustomDataSchema extends ExpTrackerDataSchema {
   label: foundry.data.fields.StringField<string, string, true, false, false>,
   value: foundry.data.fields.NumberField<number, number, true, false, false>,
+  amount: foundry.data.fields.NumberField<number, number, true, false, true>,
 }
--- src/scripts/config/circumstance-modifiers.ts
+++ src/scripts/config/circumstance-modifiers.ts
@@ -8,14 +8,14 @@ export const CircumstanceModifierGroups = Object.freeze({
         label: "PTR2E.CircumstanceModifiers.Modifiers.MajorNPCDefeated.label",
         hint: "PTR2E.CircumstanceModifiers.Modifiers.MajorNPCDefeated.hint",
         name: "combat.major-npc-defeated",
-        checked: false,
+        amount: 0,
         value: 2
       },
       {
         id: "won-encounter",
         label: "PTR2E.CircumstanceModifiers.Modifiers.WonEncounter.label",
         name: "combat.won-encounter",
-        checked: false,
+        amount: 0,
         value: 0.5
       },
       {
@@ -23,7 +23,7 @@ export const CircumstanceModifierGroups = Object.freeze({
         label: "PTR2E.CircumstanceModifiers.Modifiers.ThwartedEvilPlans.label",
         hint: "PTR2E.CircumstanceModifiers.Modifiers.ThwartedEvilPlans.hint",
         name: "combat.thwarted-evil-plans",
-        checked: false,
+        amount: 0,
         value: 0.4
       },
       {
@@ -31,7 +31,7 @@ export const CircumstanceModifierGroups = Object.freeze({
         label: "PTR2E.CircumstanceModifiers.Modifiers.ImpressiveStrategy.label",
         hint: "PTR2E.CircumstanceModifiers.Modifiers.ImpressiveStrategy.hint",
         name: "combat.impressive-strategy",
-        checked: false,
+        amount: 0,
         value: 0.4
       },
       {
@@ -39,7 +39,7 @@ export const CircumstanceModifierGroups = Object.freeze({
         label: "PTR2E.CircumstanceModifiers.Modifiers.ImpressedMajorNPC.label",
         hint: "PTR2E.CircumstanceModifiers.Modifiers.ImpressedMajorNPC.hint",
         name: "combat.impressed-major-npc",
-        checked: false,
+        amount: 0,
         value: 0.2
       },
       {
@@ -47,7 +47,7 @@ export const CircumstanceModifierGroups = Object.freeze({
         label: "PTR2E.CircumstanceModifiers.Modifiers.EntertainTheCrowd.label",
         hint: "PTR2E.CircumstanceModifiers.Modifiers.EntertainTheCrowd.hint",
         name: "combat.entertain-the-crowd",
-        checked: false,
+        amount: 0,
         value: 0.2
       },
       {
@@ -55,7 +55,7 @@ export const CircumstanceModifierGroups = Object.freeze({
         label: "PTR2E.CircumstanceModifiers.Modifiers.AllySaved.label",
         hint: "PTR2E.CircumstanceModifiers.Modifiers.AllySaved.hint",
         name: "combat.ally-saved",
-        checked: false,
+        amount: 0,
         value: 0.1
       },
       {
@@ -63,14 +63,14 @@ export const CircumstanceModifierGroups = Object.freeze({
         label: "PTR2E.CircumstanceModifiers.Modifiers.SucceedAgainstOdds.label",
         hint: "PTR2E.CircumstanceModifiers.Modifiers.SucceedAgainstOdds.hint",
         name: "combat.succeed-against-odds",
-        checked: false,
+        amount: 0,
         value: 0.2
       },
       {
         id: "capture-new-species",
         label: "PTR2E.CircumstanceModifiers.Modifiers.CaptureNewSpecies.label",
         name: "combat.capture-new-species",
-        checked: false,
+        amount: 0,
         value: 0.1
       },
       {
@@ -78,28 +78,28 @@ export const CircumstanceModifierGroups = Object.freeze({
         label: "PTR2E.CircumstanceModifiers.Modifiers.DestroyedPublicInfrastructure.label",
         hint: "PTR2E.CircumstanceModifiers.Modifiers.DestroyedPublicInfrastructure.hint",
         name: "combat.destroyed-public-infrastructure",
-        checked: false,
+        amount: 0,
         value: -0.15
       },
       {
         id: "lost-encounter",
         label: "PTR2E.CircumstanceModifiers.Modifiers.LostEncounter.label",
         name: "combat.lost-encounter",
-        checked: false,
+        amount: 0,
         value: -0.3
       },
       {
         id: "half-party-fainted",
         label: "PTR2E.CircumstanceModifiers.Modifiers.HalfPartyFainted.label",
         name: "combat.half-party-fainted",
-        checked: false,
+        amount: 0,
         value: -0.3
       },
       {
         id: "party-defeated",
         label: "PTR2E.CircumstanceModifiers.Modifiers.PartyDefeated.label",
         name: "combat.party-defeated",
-        checked: false,
+        amount: 0,
         value: -0.5
       }
     ]
@@ -113,7 +113,7 @@ export const CircumstanceModifierGroups = Object.freeze({
         label: "PTR2E.CircumstanceModifiers.Modifiers.MajorInfluencer.label",
         hint: "PTR2E.CircumstanceModifiers.Modifiers.MajorInfluencer.hint",
         name: "social.major-influencer",
-        checked: false,
+        amount: 0,
         value: 1
       },
       {
@@ -121,7 +121,7 @@ export const CircumstanceModifierGroups = Object.freeze({
         label: "PTR2E.CircumstanceModifiers.Modifiers.AvoidFight.label",
         hint: "PTR2E.CircumstanceModifiers.Modifiers.AvoidFight.hint",
         name: "social.avoid-fight",
-        checked: false,
+        amount: 0,
         value: 0.5
       },
       {
@@ -129,7 +129,7 @@ export const CircumstanceModifierGroups = Object.freeze({
         label: "PTR2E.CircumstanceModifiers.Modifiers.ConvincingStory.label",
         hint: "PTR2E.CircumstanceModifiers.Modifiers.ConvincingStory.hint",
         name: "social.convincing-story",
-        checked: false,
+        amount: 0,
         value: 0.3
       },
       {
@@ -137,7 +137,7 @@ export const CircumstanceModifierGroups = Object.freeze({
         label: "PTR2E.CircumstanceModifiers.Modifiers.VeryPersuasive.label",
         hint: "PTR2E.CircumstanceModifiers.Modifiers.VeryPersuasive.hint",
         name: "social.very-persuasive",
-        checked: false,
+        amount: 0,
         value: 0.3
       },
       {
@@ -145,7 +145,7 @@ export const CircumstanceModifierGroups = Object.freeze({
         label: "PTR2E.CircumstanceModifiers.Modifiers.WonArgument.label",
         hint: "PTR2E.CircumstanceModifiers.Modifiers.WonArgument.hint",
         name: "social.won-argument",
-        checked: false,
+        amount: 0,
         value: 0.3
       },
       {
@@ -153,7 +153,7 @@ export const CircumstanceModifierGroups = Object.freeze({
         label: "PTR2E.CircumstanceModifiers.Modifiers.AwesomeRoleplay.label",
         hint: "PTR2E.CircumstanceModifiers.Modifiers.AwesomeRoleplay.hint",
         name: "social.awesome-roleplay",
-        checked: false,
+        amount: 0,
         value: 0.3
       },
       {
@@ -161,7 +161,7 @@ export const CircumstanceModifierGroups = Object.freeze({
         label: "PTR2E.CircumstanceModifiers.Modifiers.MadeTheTableLaugh.label",
         hint: "PTR2E.CircumstanceModifiers.Modifiers.MadeTheTableLaugh.hint",
         name: "social.made-the-table-laugh",
-        checked: false,
+        amount: 0,
         value: 0.3
       },
       {
@@ -169,7 +169,7 @@ export const CircumstanceModifierGroups = Object.freeze({
         label: "PTR2E.CircumstanceModifiers.Modifiers.QuickWitted.label",
         hint: "PTR2E.CircumstanceModifiers.Modifiers.QuickWitted.hint",
         name: "social.quick-witted",
-        checked: false,
+        amount: 0,
         value: 0.3
       },
       {
@@ -177,13 +177,13 @@ export const CircumstanceModifierGroups = Object.freeze({
         label: "PTR2E.CircumstanceModifiers.Modifiers.SucceedAgainstOdds.label",
         hint: "PTR2E.CircumstanceModifiers.Modifiers.SucceedAgainstOdds.hint",
         name: "social.succeeed-against-odds",
-        checked: false,
+        amount: 0,
       },
       {
         id: "capture-new-species",
         label: "PTR2E.CircumstanceModifiers.Modifiers.CaptureNewSpecies.label",
         name: "social.capture-new-species",
-        checked: false,
+        amount: 0,
         value: 0.1
       },
       {
@@ -191,7 +191,7 @@ export const CircumstanceModifierGroups = Object.freeze({
         label: "PTR2E.CircumstanceModifiers.Modifiers.OffendedNPC.label",
         hint: "PTR2E.CircumstanceModifiers.Modifiers.OffendedNPC.hint",
         name: "social.offended-npc",
-        checked: false,
+        amount: 0,
         value: -0.2
       }
     ]
@@ -204,7 +204,7 @@ export const CircumstanceModifierGroups = Object.freeze({
         id: "found-goal",
         label: "PTR2E.CircumstanceModifiers.Modifiers.FoundGoal.label",
         name: "exploration.found-goal",
-        checked: false,
+        amount: 0,
         value: 0.3
       },
       {
@@ -212,21 +212,21 @@ export const CircumstanceModifierGroups = Object.freeze({
         label: "PTR2E.CircumstanceModifiers.Modifiers.SuccessfulCamp.label",
         hint: "PTR2E.CircumstanceModifiers.Modifiers.SuccessfulCamp.hint",
         name: "exploration.successful-camp",
-        checked: false,
+        amount: 0,
         value: 0.3
       },
       {
         id: "clear-blocked-path",
         label: "PTR2E.CircumstanceModifiers.Modifiers.ClearBlockedPath.label",
         name: "exploration.clear-blocked-path",
-        checked: false,
+        amount: 0,
         value: 0.3
       },
       {
         id: "solve-puzzle",
         label: "PTR2E.CircumstanceModifiers.Modifiers.SolvePuzzle.label",
         name: "exploration.solve-puzzle",
-        checked: false,
+        amount: 0,
         value: 0.3
       },
       {
@@ -234,7 +234,7 @@ export const CircumstanceModifierGroups = Object.freeze({
         label: "PTR2E.CircumstanceModifiers.Modifiers.DoSkillChallenge.label",
         hint: "PTR2E.CircumstanceModifiers.Modifiers.DoSkillChallenge.hint",
         name: "exploration.do-skill-challenge",
-        checked: false,
+        amount: 0,
         value: 0.3
       },
       {
@@ -242,14 +242,14 @@ export const CircumstanceModifierGroups = Object.freeze({
         label: "PTR2E.CircumstanceModifiers.Modifiers.SucceedAgainstOdds.label",
         hint: "PTR2E.CircumstanceModifiers.Modifiers.SucceedAgainstOdds.hint",
         name: "exploration.succeed-against-odds",
-        checked: false,
+        amount: 0,
         value: 0.2
       },
       {
         id: "capture-new-species",
         label: "PTR2E.CircumstanceModifiers.Modifiers.CaptureNewSpecies.label",
         name: "exploration.capture-new-species",
-        checked: false,
+        amount: 0,
         value: 0.1
       }
     ]
@@ -262,28 +262,28 @@ export const CircumstanceModifierGroups = Object.freeze({
         id: "place-first",
         label: "PTR2E.CircumstanceModifiers.Modifiers.PlaceFirst.label",
         name: "contests.place-first",
-        checked: false,
+        amount: 0,
         value: 1
       },
       {
         id: "place-second",
         label: "PTR2E.CircumstanceModifiers.Modifiers.PlaceSecond.label",
         name: "contests.place-second",
-        checked: false,
+        amount: 0,
         value: 0.5
       },
       {
         id: "place-third",
         label: "PTR2E.CircumstanceModifiers.Modifiers.PlaceThird.label",
         name: "contests.place-third",
-        checked: false,
+        amount: 0,
         value: 0.25
       },
       {
         id: "win-introduction",
         label: "PTR2E.CircumstanceModifiers.Modifiers.WinIntroduction.label",
         name: "contests.win-introduction",
-        checked: false,
+        amount: 0,
         value: 0.2
       },
       {
@@ -291,7 +291,7 @@ export const CircumstanceModifierGroups = Object.freeze({
         label: "PTR2E.CircumstanceModifiers.Modifiers.TrickOpponent.label",
         hint: "PTR2E.CircumstanceModifiers.Modifiers.TrickOpponent.hint",
         name: "contests.trick-opponent",
-        checked: false,
+        amount: 0,
         value: 0.2
       },
       {
@@ -299,14 +299,14 @@ export const CircumstanceModifierGroups = Object.freeze({
         label: "PTR2E.CircumstanceModifiers.Modifiers.TopPerformer.label",
         hint: "PTR2E.CircumstanceModifiers.Modifiers.TopPerformer.hint",
         name: "contests.top-performer",
-        checked: false,
+        amount: 0,
         value: 0.2
       },
       {
         id: "last-place",
         label: "PTR2E.CircumstanceModifiers.Modifiers.LastPlace.label",
         name: "contests.last-place",
-        checked: false,
+        amount: 0,
         value: -0.1
       }
     ]
@@ -319,28 +319,28 @@ export const CircumstanceModifierGroups = Object.freeze({
         id: "place-first",
         label: "PTR2E.CircumstanceModifiers.Modifiers.PlaceFirst.label",
         name: "pokeathlons.place-first",
-        checked: false,
+        amount: 0,
         value: 1
       },
       {
         id: "place-second",
         label: "PTR2E.CircumstanceModifiers.Modifiers.PlaceSecond.label",
         name: "pokeathlons.place-second",
-        checked: false,
+        amount: 0,
         value: 0.5
       },
       {
         id: "place-third",
         label: "PTR2E.CircumstanceModifiers.Modifiers.PlaceThird.label",
         name: "pokeathlons.place-third",
-        checked: false,
+        amount: 0,
         value: 0.25
       },
       {
         id: "win-any-section",
         label: "PTR2E.CircumstanceModifiers.Modifiers.WinAnySection.label",
         name: "pokeathlons.win-any-section",
-        checked: false,
+        amount: 0,
         value: 0.2
       },
       {
@@ -348,14 +348,14 @@ export const CircumstanceModifierGroups = Object.freeze({
         label: "PTR2E.CircumstanceModifiers.Modifiers.SucceedAgainstOdds.label",
         hint: "PTR2E.CircumstanceModifiers.Modifiers.SucceedAgainstOdds.hint",
         name: "pokeathlons.succeed-against-odds",
-        checked: false,
+        amount: 0,
         value: 0.2
       },
       {
         id: "last-place",
         label: "PTR2E.CircumstanceModifiers.Modifiers.LastPlace.label",
         name: "pokeathlons.last-place",
-        checked: false,
+        amount: 0,
         value: -0.1
       }
     ]
@@ -369,7 +369,7 @@ export const CircumstanceModifierGroups = Object.freeze({
         label: "PTR2E.CircumstanceModifiers.Modifiers.BabySteps.label",
         hint: "PTR2E.CircumstanceModifiers.Modifiers.BabySteps.hint",
         name: "other.baby-steps",
-        checked: false,
+        amount: 0,
         value: 1
       }
     ]
@@ -391,7 +391,7 @@ export interface CircumstanceModifier {
   id: string;
   label: string;
   hint?: string;
-  checked: boolean;
+  amount: number;
   value: number;
   name: string;
 }
