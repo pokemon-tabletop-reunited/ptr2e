0bacb5997f6e52c9a0e2307f61d152dc0a3756b8
Ashe <ashe@soaringnetwork.com>
2025-09-19 14:59:35 +0200

    Fix Alterations in Placeholder based Traits not getting resolved.
    Resolves #1957

--- src/module/data/models/trait.ts
+++ src/module/data/models/trait.ts
@@ -71,6 +71,19 @@ class Trait {
           }
         }) as string | number
       })
+      if('alterations' in c && Array.isArray(c.alterations) && c.alterations.length > 0) {
+        c.updateSource({
+          alterations: c.alterations.map(a => ({...a, value: c.resolveValue(a.value, a.value, {
+            evaluate: false, resolvables: {
+              actor,
+              item,
+              effect,
+              attack,
+              trait: { value: this.value ?? "" }
+            }
+          }) as string | number}))
+        })
+      }
     })
     return effect;
   }
