de87e49de1902e31e82f66903dc26fbde1cb48ad
Ashe <dylan@fampiera.nl>
2026-05-23 16:27:57 +0200

    Simplify document creation logic in base.ts
    Removed unnecessary checks for unconnected roots and perks in document creation.

--- src/module/actor/base.ts
+++ src/module/actor/base.ts
@@ -2482,7 +2482,6 @@ class ActorPTR2e<
   ) {
     super._onCreateDescendantDocuments(parent, collection, documents, results, options, userId);
     if (game.users.activeGM?.id !== game.user.id) return;
-    if (!this.unconnectedRoots.length) return;
 
     function isEffect(
       collection: "effects" | "items",
@@ -2521,10 +2520,6 @@ class ActorPTR2e<
         });
       }
     }
-
-    const perks = documents.filter((d) => d.type === "perk") as PerkPTR2e[];
-    if (!perks.length) return;
-
   }
 
   protected override _onDeleteDescendantDocuments(
